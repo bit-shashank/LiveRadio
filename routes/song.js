ada09e338d49385ad73be7f4368fb08300733e1c
bit-shashank <shashankcs083@gmail.com>
2021-03-15 16:29:52 +0530

    Initial Commit

---
+++ routes/song.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+const multer=require('multer');
+const Song=require('../models/songs');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname );
+    }
+})
+const upload=multer({storage:storage});
+
+router.get("/upload",(req,res)=>{
+    res.render("upload");
+});
+
+router.post("/upload",upload.single('song'),async (req,res)=>{
+    const file=req.file;
+
+    const song=new Song({
+        path:file.path,
+        name:req.body.name,
+    });
+
+    try{
+        await song.save();
+        console.log("New Song Uploaded");
+        res.send("Song Uploaded Succesfully");
+
+        let songs=await Song.find({});
+        console.log("Total Songs:"+songs.length);
+    }
+    catch(e){
+        console.log(e);
+        res.send("Error Occured");
+    }
+});
+
+
+router.get('/all',async (req,res)=>{
+    let songs= await Song.find().sort({ _id: -1 });
+    res.send(songs);
+})
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,100 @@
+const express= require('express');
+const app=express()
+const fs=require('fs');
+const path=require('path');
+const Throttle = require('throttle');  
+let mongoose = require('mongoose');
+let Songs=require('./models/songs');
+const  {File} = require("node-taglib-sharp");
+const Queue=require("./src/Queue");
+
+
+//Conneting to database
+
+let connURL="mongodb+srv://loneCoder:QWERTY1234@cluster0-avdpm.mongodb.net/songDB?retryWrites=true&w=majority";
+mongoose.connect(connURL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+});
+
+const con = mongoose.connection;
+con.on("open", () => {
+	console.log("Connected To Database");
+});
+
+
+
+//Views 
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+
+
+const songsRouter = require("./routes/song");
+const songs = require('./models/songs');
+app.use('/song',songsRouter);
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
+})
+
+app.get('/stream', function(req,res){
+    res.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+    });
+    writables.push(res);
+});
+
+const server=app.listen(process.env.PORT | 9000, ()=>{
+    console.log("Server Started");
+});// starting a server at port 9000 
+
+
+
+
+writables=[]
+
+startStreaming();
+
+
+async function startStreaming(){
+    songQueue=new Queue();
+    await songQueue.loadSongs();
+    songQueue.nextSong();
+    // console.log(songQueue.songs);
+    playSong();
+}
+
+function playSong(){
+    let songPath=songQueue.currentSong.path;
+    let songLocation=path.dirname(__filename)+ "\\" + songPath;
+
+    try{
+        // console.log(songQueue.songs);
+        let songFile=File.createFromPath(songLocation);
+        const bitRate=songFile.properties.audioBitrate;
+        let musicStream=fs.createReadStream(songLocation);
+        const throttle = new Throttle(bitRate*1000 / 8);
+
+        console.log("Now Playing "+ songPath);
+        musicStream.pipe(throttle).on('data', (chunk) => {
+            for (const writable of writables) {
+              writable.write(chunk);
+        }});
+        
+        musicStream.on('end',()=>{
+            songQueue.loadSongs();
+            songQueue.nextSong();
+            playSong();
+        });
+    }
+    catch(e){
+        console.log("Unable to play "+songPath);
+        songQueue.loadSongs();
+        songQueue.nextSong();
+        playSong();
+    }
+
+}
